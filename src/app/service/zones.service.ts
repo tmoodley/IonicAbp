2c4f38048db37e30e55df6af7287d33bc18fb823
Ty Moodley <ty.moodley@outlook.com>
2018-10-26 06:51:21 -0400

    added authorization to header with token.

--- src/app/service/zones.service.ts
+++ src/app/service/zones.service.ts
@@ -2,21 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient, } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AppSettings } from '../../AppSettings';
+import { UtilService } from './util.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ZonesService {
-  constructor(public http: HttpClient) {}
+  constructor(public http: HttpClient, public utilService: UtilService) {}
   getZones(): Observable<any> {
-    const options_: any = {
-      observe: 'response',
-      responseType: 'json',
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json',
-        Accept: 'application/json'
-      })
-    };
+    const options_: any = { observe: 'response', responseType: 'json', headers: new HttpHeaders(
+        {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${this.utilService.token}`
+        }
+      ) };
     return this.http.get(
       AppSettings.API_ENDPOINT + '/api/Zones/GetAll',
       options_
@@ -29,9 +29,13 @@ export class ZonesService {
       responseType: 'json',
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        Accept: 'application/json'
+        Accept: 'application/json',
+        Authorization: `Bearer ${this.utilService.token}`
       })
     };
-    return this.http.get(AppSettings.API_ENDPOINT + '/api/Zones/GetAllById/?id=' + id, options_);
+    return this.http.get(
+      AppSettings.API_ENDPOINT + '/api/Zones/GetAllById/?id=' + id,
+      options_
+    );
   }
 }
